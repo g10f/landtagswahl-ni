f2533e70a37814c77eb641f6b9f0270bd724d042
g10f <e26ce8fa4205ad32ede52a422c4cbfa57137a889>
2022-04-13 12:29:01 +0200

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,40 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const sourcemaps = require('gulp-sourcemaps');
+const rename = require("gulp-rename");
+const {parallel} = require('gulp');
+
+const config = {
+    srcCss: ['./web/scss/afd.scss'], buildCss: './web/css'
+};
+
+function buildStyles() {
+    return gulp.src(config.srcCss)
+        .pipe(sourcemaps.init())
+        .pipe(sass().on('error', sass.logError))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest(config.buildCss))
+}
+
+function buildMinStyles() {
+    return gulp.src(config.srcCss)
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(rename({extname: '.min.css'}))
+        .pipe(gulp.dest(config.buildCss))
+}
+
+function copyJavaScriptFiles() {
+    return gulp.src([
+        './node_modules/bootstrap/dist/js/bootstrap.bundle.min.js',
+        './node_modules/bootstrap/dist/js/bootstrap.bundle.min.js.map'])
+        .pipe(gulp.dest('./web/js/vendor'))
+}
+
+function copyFontFiles() {
+    return gulp.src([
+        './node_modules/bootstrap-icons/font/*.css',
+        './node_modules/bootstrap-icons/font/**//fonts/*.*',
+    ]).pipe(gulp.dest('./web/font'))
+}
+
+exports.default = parallel(buildStyles, buildMinStyles, copyJavaScriptFiles, copyFontFiles);
